a2b4ac3f609f88fad9132d84777e04c7c2fb2615
root <maytepioli>
2024-06-17 13:59:45 +0000

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - numero
+ *
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+	int numero = '0';
+		while (numero <= '9')
+		{
+			putchar(numero);
+			numero++;
+		}
+			putchar('\n');
+return (0);
+}

876d66c72261c2c5371e7734b7b289c0d0f3c0f2
maytepioli <maytepioli@gmail.com>
2024-06-18 18:31:03 +0000

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * int _abs - que calcule el valor absoluto de un número entero
+ * Return: 0
+ */
+int _abs(int n)
+{
+		if (n >= 0)
+	{
+			return (n);
+	}
+
+		else
+	{
+			return (-n);
+	}
+}

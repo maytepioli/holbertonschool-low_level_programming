57de9c48cf1078a017d014fcb6d6a4c6f03b5433
maytepioli <maytepioli@gmail.com>
2024-06-20 14:15:44 +0000

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_strlen - la funcion que cuenta los caracteres
+ *@s: string
+ * Return: 0.
+ */
+int _strlen(char *s)
+{
+
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+
+	return (len);
+}

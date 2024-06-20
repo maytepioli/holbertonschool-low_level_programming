ba8d4373002fd03d1ea0de630aa38e06d1ccadf1
maytepioli <maytepioli@gmail.com>
2024-06-20 19:56:02 +0000

    6-puts2

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string -
+ *@s: strint
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{	
+	int largo;
+
+
+	for (largo = 0; s[largo] != '\0'; largo++)
+	{
+		if (largo/2)
+
+			break;
+	}
+	
+		
+
+		_putchar('\n');
+}	
---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - imprimir
+ *@str: string
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{ 	
+	int num; 
+
+	for (num = 0; str [num] != '\0'; num++) 
+	{
+		if (num % 2 == 0)
+		{
+			_putchar(str[num]);
+		}
+	}
+		_putchar('\n');
+}

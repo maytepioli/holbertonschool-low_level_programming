a13a51ff2b6b864c879200f8d54f47f32a569170
maytepioli <maytepioli@gmail.com>
2024-06-21 15:38:08 +0000

    5-rev_string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
- * rev_string -
+ * rev_string - de vuelta la cadena
  *@s: strint
- * Return: Always 0.
+ * Return: Always 0
  */
 void rev_string(char *s)
-{	
+{
 	int largo;
-
+	int aux;
+	char a;
 
 	for (largo = 0; s[largo] != '\0'; largo++)
 	{
-		if (largo/2)
-
-			break;
 	}
-	
-		
+	for (aux = 0; aux < largo / 2; aux++)
+	{
+		a = s[aux];
+		s[aux] = s[largo - aux - 1];
+		s[largo - aux - 1] = a;
+	}
 
-		_putchar('\n');
-}	
+}

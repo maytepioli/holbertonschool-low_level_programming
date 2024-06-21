4935565158094e32da8841fb1efde3a581442aee
maytepioli <maytepioli@gmail.com>
2024-06-21 13:08:31 +0000

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,16 +12,19 @@ void puts_half(char *str)
 
 	for (num = 0; str[num] != '\0'; num++)
 	{
-		
+
 	}
 	if (num % 2 == 0)
 		i = num / 2;
 	else
-		i = (num - 1) / 2;
-
-	for ( ; str[i + 1] != '\0'; i++)
 	{
-		_putchar(str[i + 1]);
+		i = (num - 1) / 2;
+		
+	}
+
+	for (; i < num ; i++)
+	{
+		_putchar(str[i]);
 	}
 
 	_putchar('\n');

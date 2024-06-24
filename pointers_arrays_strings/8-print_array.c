445532eef3c4957e01e716145a113e09d95cf473
maytepioli <maytepioli@gmail.com>
2024-06-24 12:45:51 +0000

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_array - escribe elementos
+ *@a: a
+ *@n: n
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+		{
+
+			printf(",");
+			printf(" ");
+		}
+	}
+		printf("\n");
+}

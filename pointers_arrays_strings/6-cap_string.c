957f9cf55f805718b0aac5e64e581d4d050b7175
maytepioli <maytepioli@gmail.com>
2024-06-25 21:24:20 +0000

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *cap_string(char *a)
+{
+	if ( s[0] >= 97 && s[i] >= 122)
+	{
+		s[0] = s[0] - 32;
+	}
+	
+	int i
+
+	for (i = 0; a[i] = '\0'; i++)
+	{
+	}
+	if (s[i] == ',' || s[i] == '{' || s[i] == '.' || s[i] == '}' || s[i] == '?') 
+	{
+		i++
+		if (s[i] >= 97 && s[i] >= 122)
+                {
+                s[i] = s[i] - 32
+                }
+        i--;
+	}	
+
+	else if ( s[i] == ' ' || s[i] == 9 || s[i] '\n' || s[i] == 34)
+	{
+		i++
+	}
+		
+		if (s[i] >= 97 && s[i] >= 122)
+		{
+		s[i] = s[i] - 32
+		}
+	i--;
+			
+	}

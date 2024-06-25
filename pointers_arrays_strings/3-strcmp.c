2a9296eb740e85ca6bb8401847767f7a4e350f31
maytepioli <maytepioli@gmail.com>
2024-06-25 18:00:27 +0000

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strcmp - compara dos cadenas
+ *@s1: una cadena
+ *@s2: segunda cadena
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0')
+		return (0);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}

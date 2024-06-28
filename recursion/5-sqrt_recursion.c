8486e319ff299c37aa7eea84c5979f2f54a98f1b
maytepioli <maytepioli@gmail.com>
2024-06-28 14:34:56 +0000

    5-sqrt.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,10 +1,31 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * _sqrt_aux - aux
+ *@x: x
+ *@n: n
+ * Return: Always 0.
+ */
+int _sqrt_aux(int x, int n)
+{
+	if (x > n)
+		return (-1);
+	else if (n != (x * x))
+	x++;
+	else
+		return (x);
+	return (_sqrt_aux(x, n));
+
+}
+
+/**
+ *_sqrt_recursion - devuelva la raÃ­z cuadrada natural de un nÃ
+ *@n: n
  * Return: Always 0.
  */
 int _sqrt_recursion(int n)
 {
+	int x = 1;
 
+	return (_sqrt_aux(x, n));
+}

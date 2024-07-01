93307ec9674e3628db7970ffa6cb9262ecd2b593
maytepioli <maytepioli@gmail.com>
2024-07-01 17:32:05 +0000

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,8 +1,27 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *
- *
- *
+ * create_array - funcion
+ *@size: cantidad de memoria
+ *@c: caracter especifico
+ *Return: 0
  */
 char *create_array(unsigned int size, char c)
+{
+	unsigned int a;
+	char *caden = (char *)malloc(size * sizeof(char));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	if (caden == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < size; a++)
+	{
+		caden[a] = c;
+	}
+	return (caden);
+}

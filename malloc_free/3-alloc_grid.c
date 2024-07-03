2384a4914c984c51c905c091e1233df59eb3bb70
maytepioli <maytepioli@gmail.com>
2024-07-03 15:55:36 +0000

    3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,28 +2,25 @@
 #include <stdlib.h>
 /**
  * alloc_grid - devuelve un puntero a una matriz
- *@width: ancho de la cadena 
+ *@width: ancho de la cadena
  *@height: largo de la cadena
  *Return: grid
  */
 
 int **alloc_grid(int width, int height)
-{	
+{
 	int **grid;
 	int a;
 	int b;
+	int i;
 
-	if (width <= 0)
+	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	if (height <= 0)
-	{
-		return (NULL);
-	}
-	
-	grid = malloc(height * sizeof(int*));
-	
+
+	grid = malloc(height * sizeof(int *));
+
 	if (grid == NULL)
 	{
 		return (NULL);
@@ -32,19 +29,22 @@ int **alloc_grid(int width, int height)
 	for (a = 0; a < height; a++)
 	{
 		grid[a] = malloc(width * sizeof(int));
-		
-		if (heigth == NULL)
-		free (height);
-	}
+		if (grid[a] == NULL)
+		{
+			for (i = 0; i < a; i++)
+			{
+			free(grid[i]);
+			}
 
-	for (a = 0; a < height; a++)
-	{
+		free(grid);
+		return (NULL);
+		}
+
 		for (b = 0; b < width; b++)
 		{
 			grid[a][b] = 0;
 		}
 	}
-
-	free (grid);
 	return (grid);
 }
+

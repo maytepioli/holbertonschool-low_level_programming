7d021feaa6a642ebc4617d485de4a7790ffc17fd
maytepioli <maytepioli@gmail.com>
2024-07-04 12:54:17 +0000

    4-free_grid.c

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,11 +2,23 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
- *
+ *free_grid - libere una cuadrícula bidimensional creada previament
+ *@grid: strin bidimensional creada en el ejercicio 3
+ *@height: alto de la cuadricula
+ *Return: 0
  */
-void free_grid(int **grid, int height);
-{		
+void free_grid(int **grid, int height)
+{
+	int a;
 
+	if (grid == NULL)
+	{
+		return;
+	}
+
+	for (a = 0; a < height; a++)
+	{
+		free(grid[a]);
+	}
+	free(grid);
+}

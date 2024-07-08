b052ec0e4ad6ce158fbac2afda35aff9f82ad028
maytepioli <maytepioli@gmail.com>
2024-07-08 23:45:10 +0000

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,20 +1,31 @@
 #include "dog.h"
 #include <stdio.h>
 /**
- *
- *
+ *print_dog - imprime la funcion struct dog
+ *@d: puntero
  *
  */
 void print_dog(struct dog *d)
 {
-	if (d = NULL)
+	if (d == NULL)
 	{
 		return;
 	}
-	if (name == 0)
+	if (d->name == NULL)
 	{
-		printf("Name:(nil)\n");
+		printf("Name: (nil)\n");
 	}
-	else 
-		printf("Name: %s\n");
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+		printf("Owner: %s\n", d->owner);
 }

2750ccf5c4ebad387a7c5037ab1e8afaf31e5c55
maytepioli <maytepioli@gmail.com>
2024-07-08 17:48:52 +0000

    2-print_dog.c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d = NULL)
+	{
+		return;
+	}
+	if (name == 0)
+	{
+		printf("Name:(nil)\n");
+	}
+	else 
+		printf("Name: %s\n");
+}

9a020087ac17d9994463f783719d1da8f121cc74
maytepioli <maytepioli@gmail.com>
2024-07-11 16:02:39 +0000

    2-int_index.c

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+#include "function_pointers.h"
+/**
+ *int_index - busque un n√∫mero enter
+ *@array: array
+ *@size: tama√o
+ *@cmp: puntero a la funcion
+ *Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int len;
+
+	if (array == NULL || size <= 0)
+	{
+		return (-1);
+	}
+
+	for (len = 0; len < size; len++)
+	{
+		if (cmp(array[len]))
+		{
+			return (len);
+		}
+	}
+	return (0);
+}

7ec4393c9717f31e06b66c476946a85de25a2ec8
maytepioli <maytepioli@gmail.com>
2024-07-30 16:56:29 +0000

    4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *free_list - liberar la lista
+ *@head: encabezado del node
+ *Return: nada
+ */
+void free_list(list_t *head)
+{
+	list_t *aux;
+
+	while (head != NULL)
+	{
+		aux = head;
+		head + head->next;
+		free(aux->str);
+		free(aux);
+	}
+
+}

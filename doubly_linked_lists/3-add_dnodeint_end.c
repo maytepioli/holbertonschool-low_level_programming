3082632002f05a806640e6577137fb522883e83a
maytepioli <maytepioli@gmail.com>
2024-07-31 23:43:39 +0000

    3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 /**
@@ -9,25 +8,34 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t new_nodo = malloc(sizeof(dlistint_t));
-	dlistint_t *ultimo;
+	dlistint_t *new_nodo = malloc(sizeof(dlistint_t));
+	dlistint_t *ultimo = *head;
 
 	if (new_nodo == NULL)
 	{
+
 		return (NULL);
 	}
+
 	new_nodo->n = n;
 	new_nodo->next = NULL;
-	
-	while (
+
 
 	if (*head == NULL)
 	{
-		new_node->prev = NULL;
-		*head == new_nodo;
-		return (new_node);
+		new_nodo->prev = NULL;
+		*head = new_nodo;
+		return (new_nodo);
 	}
 
-	return(new_nodo);
+	while (ultimo->next != NULL)
+	{
+		ultimo = ultimo->next;
+	}
+
+	ultimo->next = new_nodo;
+	new_nodo->prev = ultimo;
+
+	return (new_nodo);
 
 }
